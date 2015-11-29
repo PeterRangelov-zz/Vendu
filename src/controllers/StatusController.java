db696f0fe768efd4ca4d47ceae56f85f6f23c3a4
Peter Rangelov <peter.rangelov11@gmail.com>
2015-11-29 17:47:16 -0500

    Pulse check at /status

--- src/controllers/StatusController.java
+++ src/controllers/StatusController.java
@@ -4,6 +4,7 @@ package controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +13,11 @@ public class StatusController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView getIndexPage() {
         return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/status", method = RequestMethod.GET) @ResponseBody
+    public String getStatus() {
+        return "Application running successfully.";
     }
 
 
