b8a1d64c04448d34218dc84586dc447c58211bdc
Peter Rangelov <peter.rangelov11@gmail.com>
2016-02-27 22:25:27 -0500

    get etsy shop items

--- src/controllers/EbayController.java
+++ src/controllers/EbayController.java
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class EbayController {
 
-    @RequestMapping(value = "/ebay", method = RequestMethod.POST,  produces="text/xml")
-    public String addItemToEbay(@RequestParam(value = "name") String name, @RequestParam(value = "price") double price) {
-        Item i = new Item(name, price);
-        System.out.println(i);
-        return "Created item on Ebay marketplace: " + i.toString();
-    }
+//    @RequestMapping(value = "/ebay", method = RequestMethod.POST,  produces="text/xml")
+//    public String addItemToEbay(@RequestParam(value = "name") String name, @RequestParam(value = "price") double price) {
+//        Item i = new Item(name, price);
+//        System.out.println(i);
+//        return "Created item on Ebay marketplace: " + i.toString();
+//    }
 
 
 
